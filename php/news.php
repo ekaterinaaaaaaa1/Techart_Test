74ae116b6d48b9fdb21a44689a11bda731c0137b
ekaterinaaaaaaa1 <ekaterina_kuzmina_2002@mail.ru>
2025-07-18 12:21:19 +0300

    Добавлен news.php.

---
+++ php/news.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Галактический вестник</title>
+    <link rel="stylesheet" href="../css/news_style.css"> 
+</head>
+<body>
+    <?php include 'header.php'; ?>
+    <div class="container">
+        <div class="menu">
+            <a href="index.php">
+                <span>Главная</span=>
+            </a>
+            <span> / </span>
+            <span class="menu-news-title">Возвращение этнографа</span>
+        </div>
+        <h1>Возвращение этнографа</h1>
+        <div class="news">
+            <div class="news-content news-message">
+                <span class="news-date">11.06.2412</span>
+                <h2 class="news-announce">Cегодня с Проксимы вернулась этнографическая экспедиция Джона Голдрама.</h2>
+                <p class="news-text">Сегодня с Проксимы вернулась этнографическая экспедиция под управлением профессора Джона Голдрама из Майхартского университета. 
+                    Экспедиция работала в джунглях Туманного континента и занималась изучением неконтактного племени двухвостых зюзюмов.<br><br>"Это племя называет себя 
+                    Ракатука-Тум-Тум и крайне неохотно контактирует с пришельцами", - сказал Джон Голдрам. На вопрос о том, каковы они - неконтактные зюзюмы, профессор 
+                    заявил, что неконтактные зюзюмы такие же двухвостые и скандальные как и контактные, только неконтактные.<br><br>Ждем от профессора Голдрама новых 
+                    интересных подробностей!</p>
+                <a class="button news-button" href="index.php">
+                    <img class="button-arrow" src="../img/icons/reverse_arrow.svg" alt="Стрелка"></img>
+                    <span class="button-text">Назад к новостям</span>
+                </a>
+            </div>
+            <div class="news-content">
+                <div class="news-img-block">
+                    <img class="news-img" src="../img/news/c6e252ce94fd757ae97522197c394239.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include 'footer.php'; ?>
+</body>
+</html>
